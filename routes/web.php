c967873e196039eb7465725226be022b7177246c
MemetBadut <pasekm06@gmail.com>
2025-12-18 14:55:20 +0800

    nambah tempat untuk logika kita nanti

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::middleware(['auth', 'role:' . User::ROLE_ADMIN . ',' . User::ROLE_VENDOR])->group(function () {
+    // Route::resoure('/products', ProductController::class);
 });
