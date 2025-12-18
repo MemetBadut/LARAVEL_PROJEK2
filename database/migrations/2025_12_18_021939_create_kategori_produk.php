4d8a7c38e37d5c52dc4894352a6522f4ddc134be
MemetBadut <pasekm06@gmail.com>
2025-12-18 15:30:23 +0800

    bikin tabel pivot dumlu, kita cicil beberapa hal yang bisa hari ini diselesain

--- database/migrations/2025_12_18_021939_create_kategori_produk.php
+++ database/migrations/2025_12_18_021939_create_kategori_produk.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tabel_pivot_favorites', function (Blueprint $table) {
+        Schema::create('kategori_produk', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('produk_id')->constrained('tabel_produk')->onDelete('cascade');
-            $table->unique(['user_id', 'produk_id']);
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tabel_pivot_favorites');
+        Schema::dropIfExists('kategori_produk');
     }
 };
