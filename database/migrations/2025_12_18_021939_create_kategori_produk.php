d622f7b3fe1d1b0cbc119922b5b072f86ff5ff7c
MemetBadut <pasekm06@gmail.com>
2025-12-18 15:45:19 +0800

    bikin kategori buku

--- database/migrations/2025_12_18_021939_create_kategori_produk.php
+++ database/migrations/2025_12_18_021939_create_kategori_produk.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('kategori_produk', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_kategori');
+            $table->foreignId('parent_id')->nullable()->constrained('kategori_produk')->nullOnDelete();
             $table->timestamps();
         });
     }
