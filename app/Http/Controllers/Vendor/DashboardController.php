9e9d5d0442ff71d1919fca7723f78f31d9dec7ac
MemetBadut <pasekm06@gmail.com>
2025-12-22 00:55:58 +0800

    yaa gitulah intinya

--- app/Http/Controllers/Vendor/DashboardController.php
+++ app/Http/Controllers/Vendor/DashboardController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Vendor;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class DashbboardController extends Controller
+class DashboardController extends Controller
 {
     /**
      * Display a listing of the resource.
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProdukController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Vendor\DashboardController as VendorDashboardController;
+use App\Http\Controllers\Vendor\ProdukController as VendorProductController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Admin\ProdukController as AdminProductController;
 
 // Bagian Untuk yang tidak ada yang login
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -13,12 +17,12 @@ Route::get('/produk', [ProdukController::class, 'index']);
 Route::get('/produk/{id}', [ProdukController::class, 'show']);
 
 // Route untuk User yang udah logi
-Route::middleware('auth')->group(function(){
+Route::middleware('auth')->group(function () {
     // Untuk Keranjang
     Route::get('/keranjang', [CartController::class, 'index']);
-    Route::post('/keranjang/tambah/{produkId}', [CartController::class, 'addToCart']);
-    Route::post('/keranjang/update/{produkId}', [CartController::class, 'updateCart']);
-    Route::delete('/keranjang/hapus/{itemId}', [CartController::class, 'removeFromCart']);
+    Route::post('/keranjang/tambah/{produk_id}', [CartController::class, 'addToCart']);
+    Route::post('/keranjang/update/{produk_id}', [CartController::class, 'updateCart']);
+    Route::delete('/keranjang/hapus/{produk_id}', [CartController::class, 'removeFromCart']);
 
     // Untuk checkout
     Route::get('/checkout', [CartController::class, 'checkoutForm']);
@@ -26,8 +30,25 @@ Route::middleware('auth')->group(function(){
 
     // Untuk Order
     Route::get('/orders', [OrderController::class, 'orderHistory']);
-    Route::post('/orders/', [OrderController::class, 'orderHistory']);
+    Route::post('/orders/{order_id}', [OrderController::class, 'orderHistory']);
 });
 
+Route::middleware(['auth', 'role:vendor'])
+    ->prefix('vendor')
+    ->name('vendor.')
+    ->group(function () {
 
+        Route::get('/dashboard', [VendorDashboardController::class, 'index']);
 
+        Route::resource('/products', VendorProductController::class);
+    });
+
+Route::middleware(['auth', 'role:admin'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+
+        Route::get('/dashboard', [AdminDashboardController::class, 'index']);
+
+        Route::resource('/products', AdminProductController::class);
+    });
