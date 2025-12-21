bdd965fa4d93fc40029b50b2e06417546f600f51
MemetBadut <pasekm06@gmail.com>
2025-12-22 06:46:50 +0800

    ga sengaja kehapus anjir awokawokawok

---
+++ app/Models/KategoriProduk.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KategoriProduk extends Model
+{
+    /** @use HasFactory<\Database\Factories\KategoriProdukFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'nama_kategori',
+        'parent_id',
+        'slug'
+    ];
+
+    public function produk()
+    {
+        return $this->hasMany(Produk::class, 'kategori_id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(KategoriProduk::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(KategoriProduk::class, 'parent_id');
+    }
+}
---
+++ app/Models/Order.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    /** @use HasFactory<\Database\Factories\OrderFactory> */
+    use HasFactory;
+
+    protected $fillabel = [
+        'customer_id',
+        'total_harga',
+        'order_status'
+    ];
+
+    public function customer() {
+        return $this->belongsTo(User::class, 'customer_id');
+    }
+
+    public function orderItems() {
+        return $this->hasMany(OrderItem::class, 'order_id');
+    }
+}
